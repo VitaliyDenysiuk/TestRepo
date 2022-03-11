f76fa8e13c18549cc9755c1553cf45ca0226be7f
VitaliyDenysiuk <denisuyk.vitaliy@gmail.com>
2022-03-11 13:39:35 +0200

    Save

---
+++ TestRepo-main/1TestRepository/TestRepository/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestRepository
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> num  = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                num.Add(i * i);
+            }
+            listBox1.DataSource = num;
+        }
+       
+    }
+}
